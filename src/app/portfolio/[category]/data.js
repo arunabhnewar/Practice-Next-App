96b4a18edd8ed5a5b95a1b90cc1964b14f57e85a
arunabhnewar <newardevil@gmail.com>
2023-08-20 19:06:40 +0600

    feat: portfolio page add

---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,74 @@
+export const items = {
+    applications: [
+        {
+            id: 1,
+            title: "Creative Portfolio",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+        },
+        {
+            id: 2,
+            title: "Minimal Single Product",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+        },
+        {
+            id: 3,
+            title: "Strong Together Charity",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+        }
+    ],
+    illustrations: [
+        {
+            id: 1,
+            title: "Creative Portfolio",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+        },
+        {
+            id: 2,
+            title: "Minimal Single Product",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+        },
+        {
+            id: 3,
+            title: "Strong Together Charity",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+        }
+    ],
+    websites: [
+        {
+            id: 1,
+            title: "Creative Portfolio",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "/assets/web1.png",
+        },
+        {
+            id: 2,
+            title: "Workout Website",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "/assets/web2.png",
+        },
+        {
+            id: 3,
+            title: "360 Degree Website",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "/assets/web3.png",
+        },
+        {
+            id: 4,
+            title: "MysticCC Website",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "/assets/web4.png",
+        },
+        {
+            id: 5,
+            title: "Charity Website",
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "/assets/web5.png",
+        }
+    ],
+};
