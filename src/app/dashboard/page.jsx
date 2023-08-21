4bd04fa96ba6eb60e2187d85288df65a98bb6f85
arunabhnewar <newardevil@gmail.com>
2023-08-21 18:49:11 +0600

    fix: some bug fix

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,25 @@
-const page = () => {
-  return <div>Dashboard</div>;
+/* eslint-disable @next/next/no-async-client-component */
+// "use client";
+
+// import useSWR from "swr";
+
+const getUser = async () => {
+  const data = await fetch("https://dummyjson.com/users");
+  const json = await data.json();
+  return json["users"];
 };
 
-export default page;
+const Dashboard = async () => {
+  const userData = await getUser();
+  console.log(userData);
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {userData.map((item, index) => {
+        return <p key={index}>{item?.username}</p>;
+      })}
+    </div>
+  );
+};
+
+export default Dashboard;
